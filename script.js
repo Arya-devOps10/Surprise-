249949d285a611510900cfc45e959dac985cff96
Arya <aryasuprerakmiglani@gmail.com>
2026-03-29 22:48:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ function unlock() {
 }
 
 // 2. Letter Content (Aap yahan jitna chahe likh sakte hain)
-const message = `Dear Rasmalai(Lalchand ji💗),
+const message = `Dear (),
 
-As you told me you don't like that typical bouquet 💐 or some gift type proposal so here it is this one for you something like digital letter 💌 which only you can access hehe. Before you came into my life it was kinda boring and simple I mean I was not a person I think you see me as now but yeah it took lot of things to become the guy I am now and I guess I feel like maybe everything happened for the reason maybe I am the guy right now that could be my best version to enter in your life. You know there is a theory called Red string. It says every two people in the world are connected to each other through a string. They may cross each other several time but never know. But at right time and at right place they will meet each cuz they are ment to each other. I earlier used to think this can't be true and who would even be standing at the other end of that thread holding it. But I guess I was wrong. It was you standing their holding it at the other end. Idk if things will be always good like this but I promise if it mess up I will be standing there with you fearless to fix up the mess. I can't promise you that their won't be a time in our life when you will not cry but I promise I will be standing there with you to wipe down your tears, I will be standing there with you in every situation in every happiness and problems in all circumstances kabhi thode kaand sath mein karenge toh kabhi life ke kand sath mein clean karenge. Toh meri priye Rasmalai ji do you want to become Judy of this Nick? Do you want to become Pepper Potts of this Tony Stark? Do you want to become MJ of this Peter? Do you want to become Peggy Carter of this Steve Rogers? Do you Rasmalai ji want to become partner in n crime a.k.a Girlfriend of this Arya(Authentic, Radiant, Youthful, Admirable)?`;
 
 // 3. Animation Logic
 const thread = document.getElementById('pullThread');
@@ -48,7 +47,7 @@ function startTypewriter(text) {
 
 // 4. Button Interactions
 function sayYes() {
-    alert("Yay! \n New Achivent Unlocked\n New Character Girlfriend Unlocked❤️!");
+    alert("Yay! \n New Achivent Unlocked!");
 }
 
 function moveNo() {
